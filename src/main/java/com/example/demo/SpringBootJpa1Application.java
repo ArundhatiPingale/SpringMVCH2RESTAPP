b1c25bdbe2f804495eea0efca1482be66e0cd806
ARUNDHATI <arundhati@ARUNDHATIs-MacBook-Pro.local>
2022-11-24 20:23:20 +0530

    h2 integration

---
+++ src/main/java/com/example/demo/SpringBootJpa1Application.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EntityScan("com.demo.*")
+public class SpringBootJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpa1Application.class, args);
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into Person values ( 101, 'Arundhati');
